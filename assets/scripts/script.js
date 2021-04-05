0c1b28f111f76f319d28aafd5e0eca4bb5326ff4
lewy192 <lewis.james.hill@outlook.com>
2021-04-05 18:44:52 +1000

    weather widget icon now dynamically changes to reflect day time weather events.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,108 +1,110 @@
 // defines selected elements in varibales for reuse throughout this script
-let weatherIcon = document.querySelector('.weather-icon');
-let rainfallFigure = document.querySelector('.rainfall-figure');
+let weatherIcon = document.querySelector(".weather-icon");
+let rainfallFigure = document.querySelector(".rainfall-figure");
 
 let rainfallFigureContent = rainfallFigure.textContent;
-let weatherIconSource = weatherIcon.src
+let weatherIconSource = weatherIcon.src;
 
-const weatherApiKey = '7e5922ef7f6bc85e485e53b28667f43a'
+const weatherApiKey = "7e5922ef7f6bc85e485e53b28667f43a";
+// const weatherApiUrlNormal = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=${excludeParams}&appid=${weatherApiKey}&units=metric`
+// const weatherApiUrlRain = `https://api.openweathermap.org/data/2.5/onecall/timemachine?lat=${lat}&lon=${lon}&exclude=${excludeParams}&appid=${weatherApiKey}&units=metric&dt=${yesterdayUnix}`
 
-
-
-function updateWeatherWidgets(){
+function updateWeatherWidgets() {
     var options = {
         enableHighAccuracy: true,
         timeout: 5000,
-        maximumAge: 0
+        maximumAge: 0,
     };
-    
-    function success(userPosition){
+
+    function success(userPosition) {
         let userCoordinatesResponseObject = userPosition.coords;
         let userCoordinatesObject = {
-            usersLatitude:userCoordinatesResponseObject.latitude,
-            usersLongitude:userCoordinatesResponseObject.longitude
+            usersLatitude: userCoordinatesResponseObject.latitude,
+            usersLongitude: userCoordinatesResponseObject.longitude,
         };
-        let userCoordinatesArray = [userCoordinatesObject.usersLongitude,userCoordinatesObject.usersLatitude]
-        getWeather(weatherApiKey,userCoordinatesArray,'minutely')
-        updateRainFallAmount(weatherApiKey,userCoordinatesArray,'minutely')
-    };
-    function error(err){
+        const lon = userCoordinatesObject.usersLongitude;
+        const lat = userCoordinatesObject.usersLatitude;
+        const excludeParams = "minutely";
+        // unix here is temp, could embed this function in the url itself -- for testing i will leave it as a variable here
+        const yesterdayUnix = (moment().unix() - 86400 * 4) / 1;
+        const weatherApiUrlNormal = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=${excludeParams}&appid=${weatherApiKey}&units=metric`;
+        const weatherApiUrlRain = `https://api.openweathermap.org/data/2.5/onecall/timemachine?lat=${lat}&lon=${lon}&exclude=${excludeParams}&appid=${weatherApiKey}&units=metric&dt=${yesterdayUnix}`;
+        getWeather(weatherApiUrlNormal, false);
+        getWeather(weatherApiUrlRain, true);
+    }
+    function error(err) {
         // TODO: handle errors
-        console.warn(`ERROR(${err.code}): ${err.message}`)
-        
-    };
-    navigator.geolocation.getCurrentPosition(success,error,options,true) 
-
-};
-
-function getWeather(apiKey,coords,excludeParams){
-    const lon = coords[0]
-    const lat = coords[1]
-    const weatherApiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=${excludeParams}&appid=${apiKey}&units=metric`
-
-    
-    fetch(weatherApiUrl)
-        .then(function(response) {
-            if(response.ok){
-                const responseJSON = response.json()
-                return responseJSON
-            } else {
-                // handle errors
-                console.log('handleing')
-            }
-            
-        })
-        .then(function(data){
-            console.log(data)
-            // data.current.weather[0].description  -- gives a description of weather -- use description to pick an icon
-        })
-    
-    
-};
-
-
-function useWeatherObj(weatherObj){
-    // this funciton will conaint the DOM manipulation based on the response.json()
-    console.log(weatherObj)
-
+        console.warn(`ERROR(${err.code}): ${err.message}`);
+    }
+    navigator.geolocation.getCurrentPosition(success, error, options);
 }
-function updateWeatherIcon(){
-};
 
-function updateRainFallAmount(apiKey,coords,excludeParams){
-    const lon = coords[0]
-    const lat = coords[1]
-
-    // const yesterdayUnix = (moment().unix()-(86400)*5) / 1  eg of day when rain
-    const yesterdayUnix = (moment().unix()-(86400 / 1)) / 1
-    console.log(yesterdayUnix)
-    const weatherApiUrl = `https://api.openweathermap.org/data/2.5/onecall/timemachine?lat=${lat}&lon=${lon}&exclude=${excludeParams}&appid=${apiKey}&units=metric&dt=${yesterdayUnix}`
-        
+function getWeather(weatherApiUrl, rain) {
     fetch(weatherApiUrl)
-        .then(function(response) {
-            if(response.ok){
-                const responseJSON = response.json()
-                return responseJSON
-            } else {
-                // handle errors
-                console.log('handleing')
-            }
-            
+        .then(function (response) {
+            if (response.ok) return response.json();
+            throw new Error("Weather api failed");
         })
-        .then(function(data){
-            console.log(data)
-            const hourlyDataArray = data.hourly
-            let dailyRainTotal = 0
-            for (var i= 0; i< hourlyDataArray.length;i++){
-                if('rain' in hourlyDataArray[i]){
-                    dailyRainTotal += hourlyDataArray[i].rain['1h']
-                }
-            };
-            rainfallFigure.textContent = `Rain in\nPast 24h:\n${dailyRainTotal.toFixed(2)}mm`
+        .then(function (data) {
+            rain ? updateRainfall(data) : updateWeatherIcon(data);
+        })
+        .catch(function (error) {
+            // handle error so that user knows something went wrong
+            // DON'T JUST CONSOLE LOG
         });
-    
-};
+}
 
-// getUsersLocation();
+function useWeatherObj(weatherObj) {
+    // this funciton will conaint the DOM manipulation based on the response.json()
+    console.log(weatherObj);
+}
+function updateWeatherIcon(data) {
+    const currentUserDt = data.current.dt;
+    const todaysUserSunset = data.current.sunset;
+    const isDay = currentUserDt < todaysUserSunset;
+    const currentWeatherMainDescription = data.current.weather[0].main;
+    const currentWeatherDescription = data.current.weather[0].description;
+    const currentWeatherCode = data.currnet.weather[0].id;
+    const baseSrc = "assets/images/widget-icons/";
+
+    if (isDay) {
+        if (currentWeatherCode === 800) {
+            weatherIcon.src = `${baseSrc}sun.svg`;
+            weatherIcon.alt = `${currentWeatherDescription}, sun icon.`;
+        }
+        if (currentWeatherMainDescription === "Rain") {
+            weatherIcon.src = `${baseSrc}rain.svg`;
+            weatherIcon.alt = `${currentWeatherDescription}, cloud with rain`;
+        }
+        if (currentWeatherMainDescription === "Thunderstorm") {
+            weatherIcon.src = `${baseSrc}storm.svg`;
+            weatherIcon.alt = `${currentWeatherDescription},cloud with lightning bolt and rain`;
+        }
+        if (currentWeatherCode > 802 || currentWeatherCode < 805) {
+            weatherIcon.src = `${baseSrc}overcast.svg`;
+            weatherIcon.alt = `${currentWeatherDescription}, cloud icon.`;
+        }
+        if (currentWeatherCode < 803 || currentWeatherCode > 800) {
+            weatherIcon.src = `${baseSrc}sun-and-cloud.svg`;
+            weatherIcon.alt = `${currentWeatherDescription}, sun and cloud icon together`;
+        }
+    }
+    if (!isDay) {
+    }
+}
+
+function updateRainfall(data) {
+    console.log(data);
+    const hourlyDataArray = data.hourly;
+    let dailyRainTotal = 0;
+    for (var i = 0; i < hourlyDataArray.length; i++) {
+        if ("rain" in hourlyDataArray[i]) {
+            dailyRainTotal += hourlyDataArray[i].rain["1h"];
+        }
+    }
+    rainfallFigure.textContent = `Rain in\nPast 24h:\n${dailyRainTotal.toFixed(
+        2
+    )}mm`;
+}
 
 updateWeatherWidgets();
