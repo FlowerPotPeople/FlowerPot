24f33b2da86f3dc278a9e04f03e1e7b8a5c43a1e
Clare Radtke <clare.radtke@outlook.com>
2021-04-07 21:05:32 +1000

    Set up template to generate the plant information

--- assets/scripts/goodToPlantScript.js
+++ assets/scripts/goodToPlantScript.js
@@ -7,3 +7,31 @@
 // - avg spread and height of plant,
 // - planting description?
 // - see if we can get an image
+populateSuitablePlants();
+
+function populateSuitablePlants() {
+  for (let i = 0; i < 4; i++) {
+    const el = createElementFromString(`
+    <div id="suitableForPlanting" class="card-content plants">
+      <p>{Plant name}</p>
+      <img src="assets/images/background image.jpeg" width="150" alt="image of plant">
+      <p>Prefers <span>{full sun}</span></p>
+      <p>Average height is <span>{1 meter}</span></p>
+      <p>Average span of <span>{plant name}</span> is <span>{50 cm}</span></p>
+    </div>
+    `);
+    // if (fruit || vegetable) {
+    //   const edible = createElementFromString(
+    //     `<p>**This plant bears fruit or may be edible**</p>`
+    //   );
+    //   document.getElementById("suitableForPlanting").appendChild(edible);
+    // }
+    document.getElementById("good-to-plant").appendChild(el);
+  }
+}
+
+function createElementFromString(str) {
+  const template = document.createElement("div");
+  template.innerHTML = str;
+  return template.children[0];
+}
