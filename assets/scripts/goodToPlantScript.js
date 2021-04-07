0859d14532c010eb9decf9ce5fb2a959cfdaa166
Clare Radtke <clare.radtke@outlook.com>
2021-04-07 19:48:50 +1000

    Plant cards WIP

---
+++ assets/scripts/goodToPlantScript.js
@@ -0,0 +1,9 @@
+// TODO:
+// Its a good time to plant Card:
+// template to show what is good to plant right now for the user.
+// - link to a new page to show more results
+// - given weather & month list out appropriate plants for planting with recommendations on
+// - sun/shade requirement,
+// - avg spread and height of plant,
+// - planting description?
+// - see if we can get an image
