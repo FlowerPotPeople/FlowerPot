5354df1c29d0a569ab47dde3c9f9ad5765210942
Clare Radtke <clare.radtke@outlook.com>
2021-04-13 18:14:17 +1000

    resolve HarvestHelper URL issue

--- assets/scripts/goodToPlantScript.js
+++ assets/scripts/goodToPlantScript.js
@@ -47,7 +47,7 @@ function populateFavouritePlantCard(teamFavPlants) {
 
 function callHarvestHelperApi() {
   fetch(
-    "http://harvesthelper.herokuapp.com/api/v1/plants?api_key=5bbf7f6a0d2a91ea29a665bbbd787fd5"
+    "https://harvesthelper.herokuapp.com/api/v1/plants?api_key=5bbf7f6a0d2a91ea29a665bbbd787fd5"
   )
     .then((response) => response.json())
     .then(function (data) {
@@ -69,39 +69,3 @@ function createElementFromString(str) {
   template.innerHTML = str;
   return template.children[0];
 }
-
-// Might use for a search scientific name from common name or alternate common names function
-
-// function callTrefleApi() {
-//   // call the trefle API using proxy server to avoid CORS issue
-//   fetch(
-//     `http://150.107.74.186:8083/api/v1/species?token=6wXXUz15oApy79g55Iy223zZRQZHQk7h9WbHXDFldl0`
-//   )
-//     .then((response) => response.json())
-//     .then(function (data) {
-//       console.log(data);
-//       // const plantName = document.getElementById("plant-name");
-//       // plantName.innerHTML = data.data[11]["common_name"];
-//       // const img = document.getElementById("suitablePlantImg");
-//       // img.src = data.data[11]["image_url"];
-//       // img.alt = `Image of a ${data.data[11]["common_name"]}`;
-//       fetch(
-//         `http://150.107.74.186:8083/api/v1/species/1056117?token=6wXXUz15oApy79g55Iy223zZRQZHQk7h9WbHXDFldl0`
-//       )
-//         .then((response) => response.json())
-//         .then(function (data) {
-//           console.log(data);
-//           const plantName = document.getElementById("plant-name");
-//           plantName.innerHTML = data.data["common_name"];
-//           const img = document.getElementById("suitablePlantImg");
-//           img.src = data.data["image_url"];
-//           img.alt = `Image of a ${data.data["common_name"]}`;
-//           document.getElementById("sunPreference").innerHTML =
-//             data.data.growth.light;
-//           document.getElementById("plantHeight").innerHTML =
-//             data.data.specifications.average_height.cm / 100;
-//           document.getElementById("plantSpan").innerHTML =
-//             data.data.growth.spread.cm;
-//         });
-//     });
-// }
